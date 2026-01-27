e125862126c75cb0e60cf849ddab71e19b5253e7
Mr-Sheep <24599209+Mr-Sheep@users.noreply.github.com>
2026-01-27 11:54:25 -0600

    including apple-touch-icon

--- optimize.js
+++ optimize.js
@@ -1,5 +1,6 @@
 import { optimize } from 'svgo';
 import { readFileSync, readdirSync, writeFileSync } from 'fs';
+import sharp from 'sharp';
 
 const svgFiles = readdirSync('.').filter(file => file.endsWith('.svg'));
 
@@ -9,3 +10,11 @@ svgFiles.forEach(file => {
   writeFileSync(file, data, 'utf8');
   console.log(`Optimized: ${file}`);
 });
+
+const svgBuffer = readFileSync('favicon.svg');
+sharp(svgBuffer)
+  .resize(180, 180)
+  .png()
+  .toFile('apple-touch-icon.png')
+  .then(() => console.log('converted favicon.svg to apple-touch-icon.png'))
+  .catch(err => console.error('Error converting:', err));
